3af39269363fd2d3a1794c1dc5ffffc8d32507ee
Carlos Matheu <carlos.matheu@odigeo.com>
2015-10-14 16:12:41 +0200

    jake command

---
+++ jakefile.js
@@ -0,0 +1,11 @@
+/**
+ * Created by carlos.matheu on 13/10/2015.
+ */
+(function () {
+    'use strict';
+
+    desc('default build');
+    task('default', function () {
+        console.log('\n\nBUILD OK');
+    });
+}());

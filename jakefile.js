7067e809eb6ca9dab3c526ec35bebcdbf1e26022
Carlos Matheu <carlos.matheu@odigeo.com>
2015-10-15 11:37:43 +0200

    added check version

--- jakefile.js
+++ jakefile.js
@@ -4,8 +4,21 @@
 (function () {
     'use strict';
 
+    var EXPECTED_NODE_VERSION = 'v0.10.33';
+
     desc('default build');
-    task('default', function () {
+    task('default', ['version'], function () {
         console.log('\n\nBUILD OK');
+    });
+
+    desc('check node version');
+    task('version', function () {
+        console.log('Checking node version...');
+
+        var actualVersion = process.version;
+        if(actualVersion !== EXPECTED_NODE_VERSION) {
+            fail('Incorrect node version: expected ' + EXPECTED_NODE_VERSION + ' but found ' + actualVersion);
+        }
+
     });
 }());
--- readme.md
+++ readme.md
@@ -1,9 +1,13 @@
 How To: Tabs
 ============
 
-To try the code on your computer: 
+To try the code on your computer:
 ---------------------------------
+1. Install node
+2. Install git
+3. Open a command prompt
 
 To run the build:
- 1. Install grunt 'npm install grunt -g'
- 2. Run grunt 'grunt'
+-----------------
+ 1. Install jake 'npm install jake -g'
+ 2. Run jake
